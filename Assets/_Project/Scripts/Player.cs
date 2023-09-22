bc96efe5a97f100f34a0979f77fe1f8294759965
Max Davidse <m.davidse@rubicon.nl>
2023-09-22 14:17:14 +0200

    Added boundaries for the player not to go over

---
+++ Assets/_Project/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float _playerSpeed = 5;
+    [SerializeField] float _paddingLeft, _paddingRight, _paddingTop, _paddingBottom;
+    Vector2 _rawInput;
+
+    Vector2 _minBounds;
+    Vector2 _maxBounds;
+
+    void Start()
+    {
+        InitBounds();
+    }
+    void Update()
+    {
+        Move();
+    }
+
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        _minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        _maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
+    }
+    
+    void Move()
+    {
+        Vector2 delta = _rawInput * _playerSpeed * Time.deltaTime;
+        Vector2 newPosition = new Vector2();
+        newPosition.x = Mathf.Clamp(transform.position.x + delta.x, _minBounds.x + _paddingLeft, _maxBounds.x - _paddingRight);
+        newPosition.y = Mathf.Clamp(transform.position.y + delta.y, _minBounds.y + _paddingBottom, _maxBounds.y - _paddingTop);
+        transform.position = newPosition;
+    }
+
+    void OnMove(InputValue inputValue) 
+    { 
+        _rawInput = inputValue.Get<Vector2>();
+        Debug.Log(_rawInput);
+    }
+}
