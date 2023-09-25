abfdb3f3111e54088e414a7b5bcaf6463ebaae3f
Max Davidse <m.davidse@rubicon.nl>
2023-09-25 06:19:16 +0200

    Added infinite waves

--- Assets/_Project/Scripts/EnemySpawner.cs
+++ Assets/_Project/Scripts/EnemySpawner.cs
@@ -7,6 +7,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] List<WaveConfigSO> _waveConfigs;
     [SerializeField] float _timeBetweenWaves = 0f;
+    [SerializeField] bool _isLooping;
 
     WaveConfigSO _currentWave;
 
@@ -19,20 +20,24 @@ public class EnemySpawner : MonoBehaviour
     
     IEnumerator SpawnEnemyWaves()
     {
-        foreach (WaveConfigSO wave in _waveConfigs)
+        do
         {
-            _currentWave = wave;
-
-            for (int i = 0; i < _currentWave.GetEnemyCount(); i++)
+            foreach (WaveConfigSO wave in _waveConfigs)
             {
-                Instantiate(_currentWave.GetEnemyPrefab(i),
-                    _currentWave.GetStartingWaypoint().position,
-                    Quaternion.identity,
-                    transform);
-                yield return new WaitForSeconds(_currentWave.GetRandomSpawnTime());
-            }
+                _currentWave = wave;
 
-            yield return new WaitForSeconds(_timeBetweenWaves);
+                for (int i = 0; i < _currentWave.GetEnemyCount(); i++)
+                {
+                    Instantiate(_currentWave.GetEnemyPrefab(i),
+                        _currentWave.GetStartingWaypoint().position,
+                        Quaternion.identity,
+                        transform);
+                    yield return new WaitForSeconds(_currentWave.GetRandomSpawnTime());
+                }
+
+                yield return new WaitForSeconds(_timeBetweenWaves);
+            }
         }
+        while (_isLooping);
     }
 }
